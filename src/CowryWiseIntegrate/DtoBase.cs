479ff975304b66ccfee48762aa4d3187dd87346f
Joe Izang <josephizang@gmail.com>
2021-12-16 15:43:32 +0100

    Add base dto and AccountService types and abstractions

---
+++ src/CowryWiseIntegrate/DtoBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace CowryWiseIntegrate
+{
+    public class DtoBase
+    {
+        [JsonPropertyName("errors")]
+        public string Errors { get; set; } = String.Empty;
+
+        [JsonPropertyName("message")]
+        public string Message { get; set; } = String.Empty;
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; } = String.Empty;
+    }
+}
